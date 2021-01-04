caf6af0e80ece52400d62ded478f52185ca0b007
Muhammad Baqir Jafari <muhammadbaqirjafari@gmail.com>
2021-01-04 14:40:45 +0500

    Created Signing Component

---
+++ src/components/Signin/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// Importing CSS
+import styles from './Signin.module.css'; 
+
+export const Signin = () => {
+    return (
+        <div className={styles.root}>
+            Hello From Signin
+        </div>
+    )
+}
