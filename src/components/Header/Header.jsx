b86cba5533ad074beca09dae502ad1c262a46f38
Muhammad Baqir Jafari <muhammadbaqirjafari@gmail.com>
2021-01-06 14:47:21 +0500

    Updated Header Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 // Importing CSS
 import styles from "./Header.module.css";
 
 export const Header = () => {
   return (
     <div className={styles.root}>
-      <Link to="/home" className={styles.a}>
-        <span className={styles.span}>Project Management System</span>
-      </Link>
-      <hr />
+      <div>
+        {" "}
+        <Link to="/home" className={styles.a}>
+          <span className={styles.span}>PMS</span>
+        </Link>
+      </div>
+      <div>
+        <hr />
+      </div>
     </div>
   );
 };
